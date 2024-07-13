6eb3bcd6c6512f550af5aca933994c22cec08cfb
Sadhikari2642 <109025680+Sadhikari2642@users.noreply.github.com>
2024-07-13 13:16:18 +0530

    Update sentry.server.config.ts

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,18 +1,16 @@
-// This file configures the initialization of Sentry on the server.
-// The config you add here will be used whenever the server handles a request.
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://3d627de24f5d06a1fc39000a06ca9a94@o4506813739368448.ingest.us.sentry.io/4507458386526208",
+  dsn: "https://f66b23e2ee05ab38e589a776e28e8c88@o4507564300304384.ingest.de.sentry.io/4507572058587216",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-
-  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: process.env.NODE_ENV === 'development',
 });
